45a3b3b08306dade448669c223c854761f783c2c
Morgan Caron <caron.morgan91@gmail.com>
2024-03-23 20:30:28 +0100

    Moving set_runtimes outside the target (+ fix set_kind)

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@ import Script;
 
 auto start([[maybe_unused]] std::span<const std::string_view> args) -> int
 {
-	CppUtils::Terminal::setConsoleOutputUTF8();
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+	Script::test();
+	return exitSuccess;
 }
--- xmake.lua
+++ xmake.lua
@@ -3,14 +3,23 @@ set_version("0.0.0", {build = "%Y%m%d%H%M"})
 set_license("LGPL3")
 set_languages("clatest", "cxxlatest")
 set_warnings("allextra", "pedantic", "error")
-add_cxflags("-Wconversion -Wfatal-errors")
-add_cxflags("clang::-Wno-deprecated-declarations")
+set_policy("check.auto_ignore_flags", false)
+add_cxflags("-Wconversion -Wfatal-errors", {force = true})
+add_cxflags("clang::-Wno-deprecated-declarations -Wno-unknown-attributes")
 add_cxflags("clang::-fcolor-diagnostics", "clang::-fansi-escape-codes", "gcc::-fdiagnostics-color=always")
 -- add_cxflags("-Wno-gnu-statement-expression-from-macro-expansion -Wno-gnu-statement-expression", {tools = { "clang", "gcc" })
 set_optimize("fastest")
 
 add_cxxflags("clang::-fexperimental-library", {force = true}) -- Pour avoir std::jthread
 add_cxxflags("cl::/EHsc", {force = true}) -- Pour avoir std::jthread
+
+if is_plat("windows") and is_config("cxx", "cl") and not is_plat("mingw") then
+	set_runtimes(is_mode("debug") and "MDd" or "MD")
+	add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", { public = true })
+	add_cxflags("/wd4251", {force = true}) -- ‘identifier’ : class ‘type’ needs to have dll-interface to be used by clients of class ‘type2’
+elseif is_plat("linux", "macosx") then
+	add_syslinks("pthread", "dl")
+end
 
 add_rules(
 	"mode.debug",
@@ -47,27 +56,19 @@ add_repositories("xmake-repo git@github.com:MorganCaron/xmake-repo.git")
 add_requires("CppUtils")
 
 target("Script", function()
-	set_kind("binary")
-	
-	if is_plat("windows") and is_config("cxx", "cl") and not is_plat("mingw") then
-		set_runtimes(is_mode("debug") and "MDd" or "MD")
-		add_defines("NOMINMAX", "VC_EXTRALEAN", "WIN32_LEAN_AND_MEAN", { public = true })
-		add_cxflags("/wd4251", {force = true}) -- ‘identifier’ : class ‘type’ needs to have dll-interface to be used by clients of class ‘type2’
-	elseif is_plat("linux", "macosx") then
-		add_syslinks("pthread", "dl")
-	end
+	set_kind("moduleonly")
 
-	add_packages("CppUtils")
-
-	set_policy("build.merge_archive", true)
+	add_packages("CppUtils", {public = true})
 	set_policy("build.c++.modules", true)
+	add_defines("DLL_EXPORT")
+	add_defines(is_kind("static") and "STATIC_LIB" or "SHARED_LIB")
+
 	add_files("modules/**.mpp", { public = true })
 end)
 
 target("Script-Executable", function()
 	set_kind("binary")
 	add_deps("Script")
-	set_policy("build.merge_archive", true)
 	set_policy("build.c++.modules", true)
 	add_files("src/main.cpp")
 end)
